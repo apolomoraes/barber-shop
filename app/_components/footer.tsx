2e3712335bd3a0e79ebed83ec90342b4b5f3b245
Apolo Moraes <vlogsapolo@gmail.com>
2024-01-31 16:46:18 -0300

    feat: add footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (  
+    <footer className="w-full bg-secondary px-5 py-6">
+      <p className="text-gray-400 text-xs font-bold opacity-75">© 2023 Copyright FSW Barber</p>
+    </footer>
+  );
+}
+ 
+export default Footer;
