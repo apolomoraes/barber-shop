79a53572ebf8706ab44b9485089ecf8ab972ff9b
Apolo Moraes <vlogsapolo@gmail.com>
2024-02-02 12:55:59 -0300

    feat: add initial google login

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 
 const Header = () => {
+
   return (  
     <Card>
       <CardContent className="p-5 justify-between items-center flex flex-row">
@@ -16,6 +19,7 @@ const Header = () => {
       <Button variant="outline" size="icon" className="h-8 w-8">
         <MenuIcon size={16}/>
       </Button>
+     
       </CardContent>
     </Card>
   );
