09f90cbcac8053421f15e5ee6ecae4ad032db793
Apolo Moraes <vlogsapolo@gmail.com>
2024-02-29 18:18:40 -0300

    feat: ensure confirmed bookings are shown on home page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -6,18 +6,21 @@ import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import SideMenu from "./side-menu";
+import Link from "next/link";
 
 
 const Header = () => {
   return (  
     <Card>
       <CardContent className="p-5 justify-between items-center flex flex-row">
-      <Image 
-        src="/logo.png"
-        alt="FSW Barber"
-        width={130}
-        height={22}
-      />
+        <Link href="/">
+          <Image 
+          src="/logo.png"
+          alt="FSW Barber"
+          width={130}
+          height={22}
+          />
+        </Link>
       <Sheet>
         <SheetTrigger asChild>
           <Button variant="outline" size="icon">
